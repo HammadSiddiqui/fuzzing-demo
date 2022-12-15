25153d36322548758e95d35c86337c60833e590e
Hammad Siddiqui <hammadsiddiqui@Hammads-MacBook-Air.local>
2022-12-15 18:21:49 +0300

    Made a few changes

--- fuzzer.cpp
+++ fuzzer.cpp
@@ -27,7 +27,6 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   
  // std::cout << "Input Value : \n" << Data;
   find_newline(reinterpret_cast<const char *> (Data));
- 
   return 0;  // Values other than 0 and -1 are reserved for future use. 
 }
 
