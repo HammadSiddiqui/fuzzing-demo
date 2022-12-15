7d9c01df4d0b105dfe4dfd4582439cda8e9f7231
Hammad Siddiqui <hammadsiddiqui@Hammads-MacBook-Air.local>
2022-12-15 16:23:18 +0300

    Fixing memory leak

--- fuzzer.cpp
+++ fuzzer.cpp
@@ -5,12 +5,19 @@
 size_t find_newline(const char *str)
 {
     size_t location = 0;
-  //  if(str[0] != '\0' && str[0]==' ') {
-      while (str[location] != '\n') 
-      {
-          ++location;
-      }
-  //  }
+    size_t length = strlen(str);
+//    std::cout << "Size of Char* " << length;
+
+    while (str[location] != '\n') 
+    {
+
+        ++location;
+
+        if (location == sizeof(str)) { //becoming memory safe by cutting off loop at the end of string
+          return 1;
+        }
+    }
+  
 
     return location;
 }
@@ -18,7 +25,7 @@ size_t find_newline(const char *str)
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   
-  std::cout << "Input Value : \n" << Data;
+ // std::cout << "Input Value : \n" << Data;
   find_newline(reinterpret_cast<const char *> (Data));
   
   return 0;  // Values other than 0 and -1 are reserved for future use. 
