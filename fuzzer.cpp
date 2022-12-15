ea2be294178f81ee6dc7560147544ad2e3d72439
Hammad Siddiqui <hammadsiddiqui@Hammads-MacBook-Air.local>
2022-12-15 21:29:28 +0300

    Demo

--- fuzzer.cpp
+++ fuzzer.cpp
@@ -2,20 +2,24 @@
 #include <stddef.h>
 #include <iostream>
 
+
+
+size_t someother(uint8_t x) {
+  if(x==9) {
+      return 0;
+  } 
+   return 1;
+}
+
 size_t find_newline(const char *str)
 {
     size_t location = 0;
-   // size_t length = strlen(str);
-
 
     while (str[location] != '\n') 
     {
-
         ++location;
 
-        // if (location == sizeof(str)) { //becoming memory safe by cutting off loop at the end of string
-        //   return 1;
-        // }
+
     }
   
 
@@ -26,14 +30,33 @@ size_t find_newline(const char *str)
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   
  // std::cout << "Input Value : \n" << Data;
-  find_newline(reinterpret_cast<const char *> (Data));
-  return 0;  // Values other than 0 and -1 are reserved for future use. 
+ // find_newline(reinterpret_cast<const char *> (Data));
+  someother(*Data);
+  return 0;   
 }
 
 
 // int main() 
 // {
 //   //print the index of \n 
-//   std::cout << find_newline("Hello\n");
+//   std::cout << find_newline("ma");
 
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // if (location == sizeof(str)) { //becoming memory safe by cutting off loop at the end of string
+        //   return 0;
+        // }
